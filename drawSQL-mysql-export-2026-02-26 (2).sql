49fb99dd03eaa9b7a83d56f18431c9f6c1d66726
Akuamai <akuamailive@gmail.com>
2026-02-26 17:17:24 +0100

    Add files via upload

---
+++ drawSQL-mysql-export-2026-02-26 (2).sql
@@ -0,0 +1,27 @@
+CREATE TABLE `Chambres`(
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `numero` BIGINT NOT NULL,
+    `etages` BIGINT NOT NULL,
+    `NbLits` BIGINT NOT NULL
+);
+CREATE TABLE `Lit`(
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `Occupations` BOOLEAN NOT NULL,
+    `numero_Chambres` BIGINT NOT NULL,
+    `dateEntree` DATE NOT NULL,
+    `dateSortie` DATE NOT NULL
+);
+CREATE TABLE `Classes`(
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `libelle` VARCHAR(255) NOT NULL
+);
+CREATE TABLE `Etages`(
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `numeroClasse` BIGINT NOT NULL
+);
+ALTER TABLE
+    `Etages` ADD CONSTRAINT `etages_numeroclasse_foreign` FOREIGN KEY(`numeroClasse`) REFERENCES `Classes`(`id`);
+ALTER TABLE
+    `Chambres` ADD CONSTRAINT `chambres_etages_foreign` FOREIGN KEY(`etages`) REFERENCES `Etages`(`id`);
+ALTER TABLE
+    `Lit` ADD CONSTRAINT `lit_numero_chambres_foreign` FOREIGN KEY(`numero_Chambres`) REFERENCES `Chambres`(`numero`);
