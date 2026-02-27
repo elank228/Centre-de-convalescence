e43ccf5fb22a2e531c5a732a6eb7a385ce3f0ba1
CartalTitouan <titouancartal@gmail.com>
2026-02-27 11:42:11 +0100

    Update Creation_table.sql

--- SQL/Creation_table.sql
+++ SQL/Creation_table.sql
@@ -2,14 +2,15 @@ CREATE TABLE `Chambres`(
     `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `numero` BIGINT NOT NULL,
     `etages` BIGINT NOT NULL,
-    `NbLits` BIGINT NOT NULL
+    `nbLits` BIGINT NOT NULL,
+    `numeroClasse` BIGINT NOT NULL
 );
 CREATE TABLE `Lit`(
     `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `Occupations` TINYINT NOT NULL,
     `numero_Chambres` BIGINT NOT NULL,
     `dateEntree` DATE NOT NULL,
-    `dateSortie` DATE NULL
+    `dateSortie` DATE NOT NULL,
+    `Occupation` BIGINT NOT NULL
 );
 CREATE TABLE `Classes`(
     `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -21,6 +22,8 @@ CREATE TABLE `Etages`(
 );
 ALTER TABLE
     `Etages` ADD CONSTRAINT `etages_numeroclasse_foreign` FOREIGN KEY(`numeroClasse`) REFERENCES `Classes`(`id`);
+ALTER TABLE
+    `Chambres` ADD CONSTRAINT `chambres_numeroclasse_foreign` FOREIGN KEY(`numeroClasse`) REFERENCES `Classes`(`id`);
 ALTER TABLE
     `Chambres` ADD CONSTRAINT `chambres_etages_foreign` FOREIGN KEY(`etages`) REFERENCES `Etages`(`id`);
 ALTER TABLE
