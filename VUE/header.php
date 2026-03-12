ff9f46e1d0d0f55a45eab7537692e018d3d00afe
CartalTitouan <titouancartal@gmail.com>
2026-03-12 13:13:05 +0100

    Add files via upload

---
+++ VUE/header.php
@@ -0,0 +1,3 @@
+<img src="assets\img\LOGO CENTRE DE CONVALESCENCE.png" alt="LOGO CENTRE DE CONVALESCENCE">
+<h1>Dashboard du centre de convalescence</h1>
+<!-- ajouter navbar-->
---
+++ VUE/vueTauxRemplissage.php
@@ -0,0 +1,43 @@
+<?php 
+require('MODELE/tauxRemplissage.php');
+//déclaration temporaire des variables 
+$tauxRemplissageGeneral = 100;
+$tauxRemplissageRDC = 25;
+$tauxRemplissage1erEtage = 50;
+$tauxRemplissage2iemeEtage = 0;
+$tauxRemplissageClasseEnfant = 100;
+$tauxRemplissageClasseFemme = 80;
+$tauxRemplissageClasseHomme = 60;
+$tauxRemplissageClasseVide = 40;
+$tauxRemplissageClasseMixte = 20;
+?>
+
+<div class="tauxRemplissage">
+    <h2>Taux de remplissage des chambres</h2>
+    <div class="tauxRemplissageGeneral">
+        <h3>Taux de remplissage général</h3>
+        <p><?php echo($tauxRemplissageGeneral);?>%</p>
+    </div>
+    <div class="tauxRemplissageEtage">
+        <h3>Taux de remplissage par étage</h3>
+        <h4>RDC</h4>
+        <p><?php echo($tauxRemplissageRDC);?>%</p>
+        <h4>1er étage</h4>
+        <p><?php echo($tauxRemplissage1erEtage);?>%</p>
+        <h4>2ieme étage</h4>
+        <p><?php echo($tauxRemplissage2iemeEtage);?>%</p>
+    </div>
+    <div class="tauxRemplissageClasse">
+        <h3>Taux de remplissage par classe</h3>
+        <h4>Classe Enfant</h4>
+        <p><?php echo($tauxRemplissageClasseEnfant);?>%</p>
+        <h4>Classe Femme</h4>
+        <p><?php echo($tauxRemplissageClasseEnfant);?>%</p>
+        <h4>Classe Homme</h4>
+        <p><?php echo($tauxRemplissageClasseEnfant);?>%</p>
+        <h4>Classe Vide</h4>
+        <p><?php echo($tauxRemplissageClasseEnfant);?>%</p>
+        <h4>Classe Mixte</h4>
+        <p><?php echo($tauxRemplissageClasseEnfant);?>%</p>
+    </div>
+</div>
