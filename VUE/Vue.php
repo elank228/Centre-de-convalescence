408a16a65c66d53b0c1246ff6ca68721ddb23da1
CartalTitouan <titouancartal@gmail.com>
2026-03-12 13:12:26 +0100

    Create Vue.php

---
+++ VUE/Vue.php
@@ -0,0 +1,41 @@
+<?php
+
+class Vue {
+
+    //Nom du fichier assoié à la vue
+    private $fichier;
+    //Titre de la vue (défini dans le fichier vue)
+    private $titre;
+
+    public function __construct($action) {
+        //Détermination du nom du fichier vue à partir de l'action
+        $this->fichier = 'VUE/vue'.$action.'.php';
+    }
+
+    //Génere et affiche la vue
+    public function generer($donnees) {
+        //Génération de la partie spécifique de la vue
+        $contenu = $this->genererFichier($this->fichier, $donnees);
+        //Génération du gabarit commun utilisant la parti spécifique
+        $vue = $this->genererFichier('VUE/gabarit.php', array('titre' => $this->titre, 'contenu' => $contenu));
+        //Renvoi de la vue au navigateur
+        echo $vue;
+    }
+
+    //Genère un fichier vue et renvoi le résultat produit
+    private function genererFichier($fichier, $donnees) {
+        if (file_exists($fichier)) {
+            //Rend les élements du tableau $donnes accessibles dans la vue
+            extract($donnees);
+            //Démarrage de la temporisation de sortie
+            ob_start();
+            //Inclut le fichier vue
+            //Son résultat est placé dans le tampon de sorti
+            require $fichier;
+            //Arrêt de la temporisation et renvoi du tampon de sortie
+            return ob_get_clean();
+        } else {
+            throw new Exception('Fichier '.$fichier.' introuvable');
+        }
+    }
+}
