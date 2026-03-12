5751fc884fa5b46d3b592acafbc1a9aee030b6fb
CartalTitouan <titouancartal@gmail.com>
2026-03-12 13:10:38 +0100

    Add controleurTauxRemplissage class
    Created the controleurTauxRemplissage class with initial setup.

---
+++ CONTROLEUR/controleurTauxRemplissage.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once 'MODELE/tauxRemplissage.php';
+require_once 'VUE/Vue.php';
+
+class controleurTauxRemplissage {
+    /*private $billet;
+    public function __construct() {
+        $this->billet = new Billet();
+    }
+
+    //Affiche la liste de tous les billets du blog
+    public function acceuil() {
+        $billets = $this->billet->getBillets();
+        $vue = new Vue('accueil');
+        $vue->generer(array('billets' => $billets));
+    }*/
+}
