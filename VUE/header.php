c5b004818f65d2b929d352bb2993f07aaa20c124
CartalTitouan <titouancartal@gmail.com>
2026-03-13 10:43:23 +0100

    Update header.php

--- VUE/header.php
+++ VUE/header.php
@@ -1,3 +1,37 @@
-<img src="assets\img\LOGO CENTRE DE CONVALESCENCE.png" alt="LOGO CENTRE DE CONVALESCENCE">
-<h1>Dashboard du centre de convalescence</h1>
-<!-- ajouter navbar-->
+<div class="header">
+    <img src="assets\img\LOGO CENTRE DE CONVALESCENCE.png" alt="LOGO CENTRE DE CONVALESCENCE" class="logo">
+    <h1>Dashboard du centre de convalescence</h1>
+        <div class="bg-dark">
+            <div class="container">
+                <div class="row">
+                    <nav class="col navbar navbar-expand-lg navbar-dark">
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#NavbarContent">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div id="NavbarContent" class="collapse navbar-collapse">
+                            <ul class="navbar-nav">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="index.php">Taux de remplissage</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="index.php?action=gestionChambre">Gestion des chambres</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="index.php?action=repas">Service des repas</a>
+                                </li>
+                                <!--<li class="nav-item">
+                                    <a class="nav-link" href="index.php?action=particuliers">Particuliers</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="index.php?action=societes">Sociétés</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="index.php?action=tarifs">Tarifs</a>
+                                </li>-->
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
