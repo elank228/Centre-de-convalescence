8f2cee7ee3f0070589b0cca4e3b452a046427b65
CartalTitouan <titouancartal@gmail.com>
2026-03-13 10:34:46 +0100

    Update controleurTauxRemplissage.php

--- CONTROLEUR/controleurTauxRemplissage.php
+++ CONTROLEUR/controleurTauxRemplissage.php
@@ -4,15 +4,15 @@ require_once 'MODELE/tauxRemplissage.php';
 require_once 'VUE/Vue.php';
 
 class controleurTauxRemplissage {
-    /*private $billet;
+    private $taux;
     public function __construct() {
-        $this->billet = new Billet();
+        $this->taux = new tauxRemplissage();
     }
-
+    
     //Affiche la liste de tous les billets du blog
-    public function acceuil() {
-        $billets = $this->billet->getBillets();
-        $vue = new Vue('accueil');
-        $vue->generer(array('billets' => $billets));
-    }*/
+    public function tauxRemplissage() {
+        $taux = $this->taux->tauxRemplissageGeneral();
+        $vue = new Vue('tauxRemplissage');
+        $vue->generer(array('taux' => $taux));
+    }
 }
