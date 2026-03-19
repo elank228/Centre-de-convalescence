dae24c8faabceab2e4674b06decc7f7bb5374cd4
CartalTitouan <titouancartal@gmail.com>
2026-03-19 14:20:56 +0100

    Add files via upload

---
+++ VUE/vueRepasMain.php
@@ -0,0 +1,11 @@
+<div class="serviceRepas">
+    <h2>Service des repas</h2>
+    <div class="container">
+        <div class="row">
+            <h3>Quelle étage faite vous le service ?</h3> 
+            <a href="">RDC</a>
+            <a href="">1er Étage</a>
+            <a href="">2ieme Étage</a>
+        </div>
+    </div>
+</div>
---
+++ VUE/vueRepasRDC.php
@@ -0,0 +1,20 @@
+<div class="serviceRepas">
+    <h2>Service des repas du RDC</h2>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <table>
+                    <tr>
+                        <td>
+                        001
+                        Lit 1 - Steak frite 
+                        </td>
+                        <td>
+                        Lit 2 - Burger
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
