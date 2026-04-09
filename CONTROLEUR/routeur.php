28be0b7ecdc212e30414b142c7a4a82bff30a404
Akuamai <akuamailive@gmail.com>
2026-04-09 16:31:40 +0200

    Add controllers for room management and meals

--- CONTROLEUR/routeur.php
+++ CONTROLEUR/routeur.php
@@ -1,39 +1,74 @@
 <?php
 
 require_once 'CONTROLEUR/controleurTauxRemplissage.php';
-require_once 'CONTROLEUR/controleurAccueil.php';
-//ajouter require_once pour chaque controleur
+require_once 'CONTROLEUR/controleurGestionChambre.php';
+require_once 'CONTROLEUR/controleurRepas.php';
 require_once 'VUE/Vue.php';
 
 class Routeur
 {
     private $ctrlTauxRemplissage;
-    private $ctrlAccueil;
-    //ajouter private $ pour chaque controleur
+    private $ctrlGestionChambre;
+    private $ctrlRepas;
 
     public function __construct()
     {
         $this->ctrlTauxRemplissage = new controleurTauxRemplissage();
-        $this->ctrlAccueil = new controleurAccueil();
-        //ajouter pour chaque controleur
+        $this->ctrlGestionChambre  = new controleurGestionChambre();
+        $this->ctrlRepas           = new controleurRepas();
     }
 
-    //Traite une requête entrante
     public function routerRequete()
     {
         try {
             if (isset($_GET['action'])) {
-                if ($_GET['action'] == 'x') {
-                    
-                } else if ($_GET['action'] == 'commenter') {
-                    $auteur = $this->getParametre($_POST, 'auteur');
-                    $contenu = $this->getParametre($_POST, 'contenu');
-                    $idBillet = $this->getParametre($_POST, 'id');
-                    $this->ctrlBillet->commenter($auteur, $contenu, $idBillet);
-                } else {
-                    throw new Exception('Action non valide');
+                switch ($_GET['action']) {
+
+                    // --- Gestion des chambres ---
+                    case 'gestionChambre':
+                        $this->ctrlGestionChambre->afficherChambres();
+                        break;
+
+                    case 'admissionForm':
+                        $this->ctrlGestionChambre->afficherFormulaireAdmission();
+                        break;
+
+                    case 'admettrePatient':
+                        $numeroLit   = $this->getParametre($_POST, 'numeroLit');
+                        $numeroClasse = $this->getParametre($_POST, 'numeroClasse');
+                        $this->ctrlGestionChambre->admettrePatient($numeroLit, $numeroClasse);
+                        break;
+
+                    case 'sortieForm':
+                        $this->ctrlGestionChambre->afficherFormulaireSortie();
+                        break;
+
+                    case 'sortirPatient':
+                        $numeroLit = $this->getParametre($_POST, 'numeroLit');
+                        $this->ctrlGestionChambre->sortirPatient($numeroLit);
+                        break;
+
+                    // --- Service des repas ---
+                    case 'repas':
+                        $this->ctrlRepas->afficherMenu();
+                        break;
+
+                    case 'repasRDC':
+                        $this->ctrlRepas->afficherRDC();
+                        break;
+
+                    case 'repasPremier':
+                        $this->ctrlRepas->afficherPremierEtage();
+                        break;
+
+                    case 'repasDeuxieme':
+                        $this->ctrlRepas->afficherDeuxiemeEtage();
+                        break;
+
+                    default:
+                        throw new Exception('Action non valide');
                 }
-            } else { //aucune action définie : affichage des taux de remplissage
+            } else {
                 $this->ctrlTauxRemplissage->tauxRemplissage();
             }
         } catch (Exception $e) {
@@ -41,14 +76,12 @@ class Routeur
         }
     }
 
-    //Affiche une erreur
     private function erreur($msgErreur)
     {
         $vue = new Vue('Erreur');
-        $vue->generer(array('msgErreur' => $msgErreur));
+        $vue->generer(['msgErreur' => $msgErreur]);
     }
 
-    //Recherche un paramètre dans un tableau
     private function getParametre($tableau, $nom)
     {
         if (isset($tableau[$nom])) {
