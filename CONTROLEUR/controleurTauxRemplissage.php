ffa29c25b229ddced71560fd006328f9d9f1c202
Akuamai <akuamailive@gmail.com>
2026-04-09 16:32:14 +0200

    Refactor controleurTauxRemplissage to use model methods

--- CONTROLEUR/controleurTauxRemplissage.php
+++ CONTROLEUR/controleurTauxRemplissage.php
@@ -1,18 +1,35 @@
 <?php
-
 require_once 'MODELE/tauxRemplissage.php';
 require_once 'VUE/Vue.php';
 
 class controleurTauxRemplissage {
-    private $taux;
+    private $modele;
+
     public function __construct() {
-        $this->taux = new tauxRemplissage();
+        $this->modele = new tauxRemplissage();
     }
-    
-    //Affiche la liste de tous les billets du blog
+
     public function tauxRemplissage() {
-        $taux = $this->taux->tauxRemplissageGeneral();
-        $vue = new Vue('tauxRemplissage');
-        $vue->generer(array('taux' => $taux));
+        $vue = new Vue('TauxRemplissage');
+        $vue->generer([
+            'tauxGeneral' => $this->fetchValeur($this->modele->tauxRemplissageGeneral()),
+            'tauxRDC'     => $this->fetchValeur($this->modele->tauxRemplissageRDC()),
+            'taux1er'     => $this->fetchValeur($this->modele->tauxRemplissage1erEtage()),
+            'taux2ieme'   => $this->fetchValeur($this->modele->tauxRemplissage2iemeEtage()),
+            'tauxEnfant'  => $this->fetchValeur($this->modele->tauxRemplissageEnfant()),
+            'tauxFemme'   => $this->fetchValeur($this->modele->tauxRemplissageFemme()),
+            'tauxHomme'   => $this->fetchValeur($this->modele->tauxRemplissageHomme()),
+            'tauxVide'    => $this->fetchValeur($this->modele->tauxRemplissageVide()),
+            'tauxMixte'   => $this->fetchValeur($this->modele->tauxRemplissageMixte()),
+        ]);
+    }
+
+    // Extrait la première valeur d'un résultat PDO
+    private function fetchValeur($result) {
+        if ($result) {
+            $row = $result->fetch(PDO::FETCH_NUM);
+            return $row ? $row[0] : '-';
+        }
+        return '-';
     }
 }
